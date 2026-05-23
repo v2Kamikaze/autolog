7fb6242a1098c2afca725d8677504466792f3224
Victor Torres <victortorrescc@alu.ufc.br>
2026-05-23 05:58:38 -0300

    feat: add car Type field and MaintenanceCount helper

--- internal/persistence/entities/car.go
+++ internal/persistence/entities/car.go
@@ -3,8 +3,16 @@ package entities
 type Car struct {
 	ID           int
 	Title        string
+	Type         string
 	KM           int
 	Maintenances []*Maintenance
+}
+
+func (c *Car) MaintenanceCount() int {
+	if c.Maintenances == nil {
+		return 0
+	}
+	return len(c.Maintenances)
 }
 
 func (c *Car) TotalCost() int {
